8fbbf5a5887bd2f42a17d7fb18b987b71b6a988b
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-15 22:47:49 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,24 @@ variable "region" {
   type    = string
   default = "west europe"
 }
+
+variable "AZURE_SUBSCRIPTION_ID" {
+  description = "The Azure Subscription ID"
+  type        = string
+}
+
+variable "AZURE_CLIENT_ID" {
+  description = "The Azure Client ID"
+  type        = string
+}
+
+variable "AZURE_CLIENT_SECRET" {
+  description = "The Azure Client Secret"
+  type        = string
+  sensitive   = true  # لأن هذا سر يجب أن يكون محمي
+}
+
+variable "AZURE_TENANT_ID" {
+  description = "The Azure Tenant ID"
+  type        = string
+}
