ae3707321f2ca5cb1da423a7cf6192efb7d744a1
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-16 21:58:35 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
   subscription_id = var.Subscription_id
 }
 
-# جلب معرف Resource Group إذا كان موجودًا
+
 data "azurerm_resource_group" "existing_rg" {
   name = "myResourceGroupTR"
 }
 
-# إنشاء Resource Group فقط إذا لم تكن موجودة
+
 resource "azurerm_resource_group" "my_rg" {
   count = length(data.azurerm_resource_group.existing_rg.id) > 0 ? 0 : 1
   
@@ -20,13 +20,13 @@ resource "azurerm_resource_group" "my_rg" {
   }
 }
 
-# جلب معرف Container Registry إذا كان موجودًا
+
 data "azurerm_container_registry" "existing_acr" {
   name                = "myacrTR202"
   resource_group_name = "myResourceGroupTR"
 }
 
-# إنشاء Container Registry فقط إذا لم يكن موجودًا
+
 resource "azurerm_container_registry" "my_acr" {
   count = length(data.azurerm_container_registry.existing_acr.id) > 0 ? 0 : 1
 
