f41061ce79ab89a701bb350d0b83d8b7730db264
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-16 19:27:24 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,27 @@
 provider "azurerm" {
   features {}
-
   subscription_id = var.Subscription_id
+}
+
+# جلب معرف Resource Group إذا كان موجودًا
+data "azurerm_resource_group" "existing_rg" {
+  name = "myResourceGroupTR"
 }
 
 resource "azurerm_resource_group" "my_rg" {
   name     = "myResourceGroupTR"
-  location = "west europe"  
+  location = "west europe"
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
 }
 
+# جلب معرف Container Registry إذا كان موجودًا
+data "azurerm_container_registry" "existing_acr" {
+  name                = "myacrTR202"
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+}
 
 resource "azurerm_container_registry" "my_acr" {
   name                = "myacrTR202"
@@ -23,7 +36,8 @@ resource "azurerm_container_registry" "my_acr" {
   tags = {
     environment = "production"
   }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
 }
-
-
-
