8e8c976a6d5a1788aed428556caa366ef397d7dc
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-16 14:33:59 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "my_rg" {
 
 
 resource "azurerm_container_registry" "my_acr" {
-  name                = "myacr"
+  name                = "myacr-1"
   resource_group_name = azurerm_resource_group.my_rg.name
   location            = azurerm_resource_group.my_rg.location
   sku                 = "Basic"
