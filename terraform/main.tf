0ebb86b5a0fcaec8aeed8b071f3679d77e6c1c6f
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-16 02:31:34 +0200

    mm

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,9 @@
 provider "azurerm" {
   features {}
-  AZURE_SUBSCRIPTION_ID = "2204702f-2344-4ad7-acc5-63b9daea47de"
-  AZURE_CLIENT_ID       = "585480eb-4228-45fe-afa7-2485eb4b8687"
-  AZURE_TENANT_ID       = "19fe7ed7-01ca-4ae6-9cb4-3d75b5264420"
+
+  subscription_id = "2204702f-2344-4ad7-acc5-63b9daea47de"
+  tenant_id       = "19fe7ed7-01ca-4ae6-9cb4-3d75b5264420"
+  client_id       = "585480eb-4228-45fe-afa7-2485eb4b8687"
 }
 
 resource "azurerm_resource_group" "my_rg" {
