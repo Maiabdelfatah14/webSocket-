92e2d7bf2076ce7c1b83645c676e29d8d57dc922
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-16 17:00:39 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,42 @@
 provider "azurerm" {
   features {}
-
-  subscription_id = var.Subscription_id
+    subscription_id = var.Subscription_id
 }
 
-resource "azurerm_resource_group" "my_rg" {
-  name     = "myResourceGroup-2"
-  location = "west europe"  
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "East US"
 }
 
-
-resource "azurerm_container_registry" "my_acr" {
-  name                = "myacrmai20255"
-  resource_group_name = azurerm_resource_group.my_rg.name
-  location            = azurerm_resource_group.my_rg.location
-  sku                 = "Basic"
-}
-
-resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  name                = "myakscluster"
-  location            = azurerm_resource_group.my_rg.location  
-  resource_group_name = azurerm_resource_group.my_rg.name    
-  dns_prefix          = "myaks"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 2
-    vm_size    = "Standard_DS2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    environment = "production"
+resource "azurerm_app_service_plan" "appserviceplan" {
+  name                = "myAppServicePlan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+  sku {
+    tier = "Basic"
+    size = "B1"
   }
 }
+
+resource "azurerm_app_service" "appservice" {
+  name                = "mywebsocketapp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+
+  site_config {
+    linux_fx_version = "DOCKER|myacrname.azurecr.io/mywebsocketapp:${var.image_tag}"
+  }
+
+  app_settings = {
+    DOCKER_REGISTRY_SERVER_URL      = "https://myacrname.azurecr.io"
+    DOCKER_REGISTRY_SERVER_USERNAME = "myacrname"
+    DOCKER_REGISTRY_SERVER_PASSWORD = "your-acr-password"
+  }
+}
+
+variable "image_tag" {}
+
+
