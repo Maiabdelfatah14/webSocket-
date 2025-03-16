aed12e960011021af442b9f796cfac08ca253368
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-17 00:13:50 +0200

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,37 +2,3 @@ variable "Subscription_id" {
   type    = string
   default = "2204702f-2344-4ad7-acc5-63b9daea47de"
 }
-
-variable "azurer_websocker_app" {
-  type = string
-  default = "fastapi-websocket"
-}
-
-variable "azurerm_linux_web_app_resource_group_name" {
-  type = string
-}
-
-variable "azurerm_linux_web_app_location" {
-  type = string
-}
-
-variable "azurerm_linux_web_app_azurerm_service_plan_id" {
-  type = string
-}
-
-variable "azurerm_linux_web_app_app_docker_image" {
-  type = string
-}
-
-variable "azurerm_linux_web_app_docker_registry_url" {
-  type = string
-}
-
-variable "docker_registry_username" {
-  type = string
-}
-
-variable "docker_registry_password" {
-  type = string
-}
-
