20bdeea239a2c93a8e8876414c17c259753a85c3
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-17 00:14:08 +0200

    Delete terraform/output.tf

--- terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "web_app_name" {
-  description = "The name of the Azure  Websocket App"
-  value       = fastapi-websocket
-}
-
-output "web_app_default_hostname" {
-  description = "The default hostname of the Azure Linux Web App"
-  value       = fastapi-websocket.default_hostname
-}
