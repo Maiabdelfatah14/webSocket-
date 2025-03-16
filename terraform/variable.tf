7a73d21a7db232a72ef83f8d07a0b81f6f0b8ea8
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-16 17:20:36 +0200

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,4 +1,5 @@
-variable "Subscription_id"{
-type= string
-default = "2204702f-2344-4ad7-acc5-63b9daea47de"
-}  
+variable "Subscription_id" {
+  type    = string
+  default = "2204702f-2344-4ad7-acc5-63b9daea47de"
+}
+
