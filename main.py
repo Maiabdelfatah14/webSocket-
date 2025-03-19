bea85bb39b898eae9b14b3566f18eb53fdbd483b
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-19 22:58:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import random
 import uvicorn
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, HTTPException
+
 
 app = FastAPI()
 
@@ -16,6 +18,13 @@ stocks = ["AAPL", "GOOGL", "AMZN", "MSFT"]
 @app.get("/")
 def home():
     return {"message": "🚀 WebSocket server is running!", "status": "healthy"}
+
+
+# for test
+@app.get("/crash")
+async def crash():
+    raise HTTPException(status_code=500, detail="Simulated Server Error")
+
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
