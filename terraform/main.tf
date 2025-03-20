2da63bec911040f24217759664fe9cb45486c8ac
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-20 23:20:38 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,8 @@ provider "azurerm" {
   subscription_id = var.Subscription_id
 }
 
-# 🔹 Fetch existing Resource Group, if not found, create it
-  resource "azurerm_resource_group" "my_rg" {
+# 🔹 Create Resource Group
+resource "azurerm_resource_group" "my_rg" {
   name     = "myResourceGroupTR"
   location = "East US"
 }
@@ -12,8 +12,8 @@ provider "azurerm" {
 # 🔹 Create ACR
 resource "azurerm_container_registry" "my_acr" {
   name                = "myacrTR202"
-  resource_group_name = "myResourceGroupTR"
-  location = "East US"
+  resource_group_name = azurerm_resource_group.my_rg.name
+  location            = azurerm_resource_group.my_rg.location
   sku                 = "Premium"
 
   identity {
@@ -28,8 +28,8 @@ resource "azurerm_container_registry" "my_acr" {
 # 🔹 Create App Service Plan
 resource "azurerm_service_plan" "app_service_plan" {
   name                = "myAppServicePlan"
-  resource_group_name = "myResourceGroupTR"
-  location = "East US"
+  resource_group_name = azurerm_resource_group.my_rg.name
+  location            = azurerm_resource_group.my_rg.location
   os_type             = "Linux"
   sku_name            = "B1"
 }
@@ -37,8 +37,8 @@ resource "azurerm_service_plan" "app_service_plan" {
 # 🔹 Create Web App
 resource "azurerm_linux_web_app" "web_app" {
   name                = "my-fastapi-websocket-app"
-  resource_group_name = "myResourceGroupTR"
-  location = "East US"
+  resource_group_name = azurerm_resource_group.my_rg.name
+  location            = azurerm_resource_group.my_rg.location
   service_plan_id     = azurerm_service_plan.app_service_plan.id
 
   site_config {
@@ -59,24 +59,24 @@ resource "azurerm_linux_web_app" "web_app" {
 # 🔹 Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "myVNet"
-  resource_group_name = "myResourceGroupTR"
-  location = "East US"
+  resource_group_name = azurerm_resource_group.my_rg.name
+  location            = azurerm_resource_group.my_rg.location
   address_space       = ["10.0.0.0/16"]
 }
 
 # 🔹 Create Subnet for Private Endpoint
 resource "azurerm_subnet" "private_subnet" {
   name                 = "myPrivateSubnet"
-  resource_group_name  = "myResourceGroupTR"
-  virtual_network_name = "myVNet"
+  resource_group_name  = azurerm_resource_group.my_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 # 🔹 Create Private Endpoint for ACR
 resource "azurerm_private_endpoint" "acr_private_endpoint" {
   name                = "acr-private-endpoint"
-  location = "East US"
-  resource_group_name = "myResourceGroupTR"
+  location            = azurerm_resource_group.my_rg.location
+  resource_group_name = azurerm_resource_group.my_rg.name
   subnet_id           = azurerm_subnet.private_subnet.id
 
   private_service_connection {
