d13890cee265ba1a7e5edd96efc41a25932239c4
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-21 21:28:10 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -218,6 +218,15 @@ resource "azurerm_network_security_rule" "deny_all" {
   network_security_group_name = azurerm_network_security_group.websocket_nsg.name
 }
 
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-vnet"
+  location            = azurerm_resource_group.my_rg.location
+  resource_group_name = azurerm_resource_group.my_rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+
 resource "azurerm_subnet" "private_subnet" {
   name                 = "private-subnet"
   resource_group_name  = azurerm_resource_group.my_rg.name
