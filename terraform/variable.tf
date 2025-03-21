9a8bd9f8460b198cee4391665073fc99a6ae7197
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-21 19:53:47 +0200

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,4 +1,11 @@
-variable "Subscription_id" {
-  type    = string
-  default = "2204702f-2344-4ad7-acc5-63b9daea47de"
-}
+#variable "Subscription_id" {
+ # type    = string
+  #default = "2204702f-2344-4ad7-acc5-63b9daea47de"}
+
+
+variable "subscription_id" {default = "2204702f-2344-4ad7-acc5-63b9daea47de"}
+variable "resource_group_name" { default = "myResourceGroupTR" }
+variable "location" { default = "East US" }
+variable "acr_name" { default = "myacrTR202" }
+variable "app_service_name" { default = "my-fastapi-websocket-app" }
+variable "app_service_plan_name" { default = "myAppServicePlan" }
