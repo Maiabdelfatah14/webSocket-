499b199404e6986842dcd0e665263b218e37d01b
Maiabdelfatah14 <157978004+Maiabdelfatah14@users.noreply.github.com>
2025-03-21 20:20:50 +0200

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,9 +1,9 @@
-#variable "Subscription_id" {
- # type    = string
-  #default = "2204702f-2344-4ad7-acc5-63b9daea47de"}
+variable "subscription_id" {
+type    = string
+default = "2204702f-2344-4ad7-acc5-63b9daea47de"}
 
 
-variable "subscription_id" {default = "2204702f-2344-4ad7-acc5-63b9daea47de"}
+#variable "subscription_id" {default = "2204702f-2344-4ad7-acc5-63b9daea47de"}
 variable "resource_group_name" { default = "myResourceGroupTR" }
 variable "location" { default = "East US" }
 variable "acr_name" { default = "myacrTR202" }
